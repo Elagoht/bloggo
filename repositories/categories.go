58e806965c3adc0bd7c27b2fcb1271fa137fc4c1
Furkan Baytekin <furkanbaytekin@gmail.com>
2025-05-07 02:59:47 +0300

    feat: CategoryRepository for database interactions with categories

---
+++ repositories/categories.go
@@ -0,0 +1,39 @@
+package repositories
+
+import (
+	"database/sql"
+
+	"github.com/Elagoht/bloggo/models"
+	"github.com/Elagoht/bloggo/utils"
+)
+
+type CategoryRepository struct {
+	dataBase *sql.DB
+}
+
+func NewCategoryRepository() *CategoryRepository {
+	return &CategoryRepository{
+		dataBase: utils.DataBase,
+	}
+}
+
+func (repository *CategoryRepository) GetAll() ([]models.Category, error) {
+	rows, err := repository.dataBase.Query("SELECT * FROM categories")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	categories := []models.Category{}
+
+	for rows.Next() {
+		var category models.Category
+		err := rows.Scan(&category.Id, &category.Name, &category.Slug, &category.Description, &category.Keywords, &category.Spot)
+		if err != nil {
+			return nil, err
+		}
+		categories = append(categories, category)
+	}
+
+	return categories, nil
+}
